d80e58ca7592f73a495c7819a2125ab3c9a8a892
Kyle Gosen <gosenkyl@msu.edu>
2017-08-07 00:13:23 -0400

    Removed un-needed annotations

--- src/main/java/com/gosenk/transit/parser/Application.java
+++ src/main/java/com/gosenk/transit/parser/Application.java
@@ -1,15 +1,9 @@
 package com.gosenk.transit.parser;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"com.gosenk.transit.api.repository"})
-@ComponentScan({"com.gosenk.transit.api"})
-@EntityScan(basePackages = "com.gosenk.transit.api")
 public class Application {
     public static void main(String[] args) {
         new SpringApplicationBuilder(Main.class).web(false).run(args);
